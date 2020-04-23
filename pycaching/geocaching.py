4ae291193697bec33da6ea67d29e1c1564638c4c
CachingFoX <cachingfox@cachingfox.org>
2020-04-23 23:06:07 +0200

    Modify termination condition in search generator

--- pycaching/geocaching.py
+++ pycaching/geocaching.py
@@ -244,8 +244,7 @@ class Geocaching(object):
             # get one page
             geocaches_table, whole_page = self._search_get_page(parameters, start_index)
 
-            if not geocaches_table:
-                # result is empty - no more caches
+            if len(geocaches_table.find_all("tr")) == 0:
                 return
 
             # prepare language-dependent mappings
@@ -257,8 +256,7 @@ class Geocaching(object):
                     for label in cache_sizes_filter_wrapper.find_all("label")
                 }
 
-            if len(geocaches_table.find_all("tr")) == 0:
-                return
+
 
             # parse caches in result
             for start_index, row in enumerate(geocaches_table.find_all("tr"), start_index):
